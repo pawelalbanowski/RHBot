b6d13643ba2b4fe7f726321618b8f72c5d07a13c
Paweł Albanowski <pawel.albanowski@gmail.com>
2023-10-22 11:18:24 +0200

    feat: FM changes

--- cogs/misc/connections.py
+++ cogs/misc/connections.py
@@ -1,5 +1,8 @@
 import pymongo
 import os
+import certifi
 
+
+ca = certifi.where()
 mongodb_uri = os.getenv('MONGODB_URI')
-mongo = pymongo.MongoClient(mongodb_uri)
+mongo = pymongo.MongoClient(mongodb_uri, tlsCAFile=ca)
--- testfile.py
+++ testfile.py
@@ -1,63 +1,13 @@
 from cogs.misc.connections import mongo
+from pprint import pprint
 
 
-db = mongo['RH']
-drivers_col = db['drivers']
+db = mongo['lq']
+test_col = db['regs_weekly']
 
 
 # reserved = get()
 
+res = test_col.find_one({'active': True})
 
-driver = {
-            "id": 435867024078536724,
-            "gt": "ESV Justin",
-            "nr": 0,
-            "league": "placement",
-            "placement": {
-                "lap_string": "1:29.773",
-                "lap_ms": 89773,
-                "finish_string": "9:06.323",
-                "finish_ms": 546323
-            },
-            "car": "Mercedes",
-            "swaps": 1,
-            "dcname": "Justintime",
-            "results": {
-                "r1": {
-                    "quali": 0,
-                    "race": 0,
-                    "fl": 0
-                },
-                "r2": {
-                    "quali": 0,
-                    "race": 0,
-                    "fl": 0
-                },
-                "r3": {
-                    "quali": 0,
-                    "race": 0,
-                    "fl": 0
-                },
-                "r4": {
-                    "quali": 0,
-                    "race": 0,
-                    "fl": 0
-                },
-                "r5": {
-                    "quali": 0,
-                    "race": 0,
-                    "fl": 0
-                },
-                "r6": {
-                    "quali": 0,
-                    "race": 0,
-                    "fl": 0
-                },
-                "r7": {
-                    "quali": 0,
-                    "race": 0,
-                    "fl": 0
-                },
-            }
-        }
-# drivers_col.insert_one(driver)
+pprint(res)
