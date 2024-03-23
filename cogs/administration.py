840b304af743f69b623655f98b6dc99d77a3d662
Paweł Albanowski <pawel.albanowski@gmail.com>
2024-03-23 16:46:57 +0100

    test: continue test1

--- cogs/administration.py
+++ cogs/administration.py
@@ -72,7 +72,7 @@ class Administration(commands.Cog):
         await msg.response.send_message(f'Processing...')
 
         message = 'Sheet has been updated!'
-        mongo_drivers = drivers_col.find({"placement": {"$exists": true}})
+        mongo_drivers = drivers_col.find({"placement": {"$exists": True}})
         for driver in mongo_drivers:
             if not driver['nr'] == 0:
                 dc_user = get(msg.guild.members, id=driver['id'])
